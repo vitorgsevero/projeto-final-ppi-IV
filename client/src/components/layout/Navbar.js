ea0338895f60c4177eea07750932b53e68bba642
vitorgsevero <vitorgsevero@gmail.com>
2019-12-01 16:26:40 -0300

    Register Form and useState Hook

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,13 +5,13 @@ export const Navbar = () => {
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <a href='index.html'>
+        <Link to='/'>
           <i className='fas fa-code'></i> DevSocial
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href='!#'>Developers</a>
+          <a href='/'>Developers</a>
         </li>
         <li>
           <Link to='/register'>Register</Link>
